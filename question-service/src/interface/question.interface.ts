f8e63acee9258e6858366f7396763b47dfe5cae8
irving11119 <irving.deboer@gmail.com>
2023-09-19 10:46:58 +0800

    Update package.json scripts

--- question-service/src/interface/question.interface.ts
+++ question-service/src/interface/question.interface.ts
@@ -1,6 +1,7 @@
 import { Document } from 'mongoose';
 
 export interface IQuestion extends Document {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   _id: string;
   title: string;
   description: string;
