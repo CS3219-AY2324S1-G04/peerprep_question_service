e90b081f39d99c30a8a12568a51bdbd2e12bfe02
irving11119 <irving.deboer@gmail.com>
2023-10-10 12:07:58 +0800

    Add unit tests for question matching endpoint

--- api/src/tests/get_question_matching.test.ts
+++ api/src/tests/get_question_matching.test.ts
@@ -4,18 +4,33 @@ import {mockData} from "./mock_data";
 import newApp from "../app";
 
 describe('Testing API endpoints for retrieving questions by match params', () => {
+
+    describe('Unsuccessful request', () => {
+        it('Should return status code 500 and an error message', async () => {
+            jest.spyOn(QuestionService.prototype, 'findByParams')
+                // @ts-ignore
+                .mockImplementationOnce(() => {
+                    throw new Error();
+                });
+            const {statusCode} = await supertest(newApp).get('/question-service/question-matching/question')
+                .query({complexity: 'Easy', categories: 'test'});
+            expect(statusCode).toBe(500);
+        });
+    });
+
     describe('GET /question-service/question-matching/question', () => {
         describe('Successful request', () => {
             it('Should return status code 200 and a list of questions in database', async () => {
                 jest.spyOn(QuestionService.prototype, 'findByParams')
                     // @ts-ignore
-                    .mockReturnValueOnce(Promise.resolve(mockData));
-                const {statusCode} = await supertest(newApp).get('/question-service/question-matching/question');
+                    .mockReturnValueOnce(Promise.resolve({}));
+                const {statusCode} = await supertest(newApp).get('/question-service/question-matching/question')
+                    .query({complexity: 'Easy', categories: ['test1', 'test2']});
                 expect(statusCode).toBe(200);
             });
         });
 
-        describe('Request missing parameters', () => {
+        describe('Request missing all parameters', () => {
             it('Should return status code 400 and an error message', async () => {
                 jest.spyOn(QuestionService.prototype, 'findByParams')
                     // @ts-ignore
@@ -27,16 +42,27 @@ describe('Testing API endpoints for retrieving questions by match params', () =>
             });
         });
 
-        describe('Unsuccessful request', () => {
-            it('Should return status code 500 and an error message', async () => {
+        describe('Request missing complexity parameter', () => {
+            it('Should return status code 400 and an error message', async () => {
                 jest.spyOn(QuestionService.prototype, 'findByParams')
                     // @ts-ignore
-                    .mockImplementation(() => {
-                        throw new Error('Error');
-                    });
-                const {statusCode} = await supertest(newApp).get('/question-service/question-matching/question');
-                expect(statusCode).toBe(500);
+                    .mockReturnValueOnce(Promise.resolve(mockData));
+                const {statusCode} = await supertest(newApp).get('/question-service/question-matching/question')
+                    .query({complexity: null, categories: ['test1', 'test2']});
+                expect(statusCode).toBe(400);
             });
         });
+
+        describe('Request missing categories parameter', () => {
+           it('Should return status code 200', async () => {
+               jest.spyOn(QuestionService.prototype, 'findByParams')
+                   // @ts-ignore
+                   .mockReturnValueOnce(Promise.resolve(mockData));
+               const {statusCode} = await supertest(newApp).get('/question-service/question-matching/question')
+                   .query({complexity: 'Easy', categories: null});
+               expect(statusCode).toBe(200);
+           })
+        });
+
     });
 });
