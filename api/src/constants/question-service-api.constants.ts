c8aa77b5e60427f5143bdfc4d1a63981202fe311
irving11119 <irving.deboer@gmail.com>
2023-10-20 15:13:10 +0800

    Move MONGO_URI to question-service-api.constants.ts file

--- api/src/constants/question-service-api.constants.ts
+++ api/src/constants/question-service-api.constants.ts
@@ -7,4 +7,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.EXPRESS_DOCKER_PORT;
+export const MONGO_URI = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_DOCKER_PORT}/questions?authSource=admin`;
 
