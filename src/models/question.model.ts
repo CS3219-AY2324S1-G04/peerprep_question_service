007aa0192fbbab40aff479b8977c42f720347fd0
irving11119 <irving.deboer@gmail.com>
2023-11-17 10:04:53 +0800

    Update question repository to prevent duplicate code

--- src/models/question.model.ts
+++ src/models/question.model.ts
@@ -7,7 +7,7 @@ import { Schema, model } from 'mongoose';
 import { IQuestion } from '../interface/question.interface';
 
 const questionModel = new Schema({
-  title: { type: String, required: [true, 'Field is required'] },
+  title: { type: String, unique: true, required: [true, 'Field is required'] },
   description: { type: String, required: [true, 'Field is required'] },
   categories: { type: Array<string>, required: [true, 'Field is required'] },
   complexity: { type: String, required: [true, 'Field is required'] },
